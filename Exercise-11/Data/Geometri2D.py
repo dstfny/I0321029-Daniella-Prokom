dbdc63fae6c85588974bd0cb5a9312a2a9d08097
Daniella <daniellastefany35@gmail.com>
2022-06-07 18:14:10 +0700

    mengerjakan Exercise-11

---
+++ Exercise-11/Data/Geometri2D.py
@@ -0,0 +1,26 @@
+# Silakan perhatikan perintah pada file 11_Modul&Paket.ipynb sebelum membuka file ini
+import math
+
+# Luas persegi panjang
+def LuasPersegiPanjang(p,l):
+    return p*l
+
+# Keliling persegi panjang
+def KelilingPersegiPanjang(p,l):
+    return 2*(p+l)
+
+# Luas bujur sangkar
+def LuasBujurSangkar (s):
+    return s*s
+    
+#Keliling bujur sangkar
+def KelilingBujurSangkar (s):
+    return 4*s
+
+# Luas segitiga
+def LuasSegitiga (a,t):
+    return (a*t)/2
+
+# Keliling segitiga
+def KelilingSegitiga (a,b,c):
+    return a+b+c
